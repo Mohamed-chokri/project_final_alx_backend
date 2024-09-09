00b21b5bb17f93ba0ad3670d5d632ea817b3b0b6
Hasona <bekoahmed20@gmail.com>
2024-09-09 14:05:23 +0300

    subscribtion work also we will make it for notification instead of redundunt chat

---
+++ client1.js
@@ -0,0 +1,46 @@
+import { io } from 'socket.io-client';
+import readline from 'readline';
+
+// Connect to the Socket.IO server
+const socket = io('http://localhost:4000');
+
+// Handle successful connection
+socket.on('connect', () => {
+    console.log('Connected to server with ID:', socket.id);
+});
+
+// Listen for new messages
+socket.on('newMessage', (data) => {
+    console.log('New message received:', data);
+});
+
+// Handle connection errors
+socket.on('connect_error', (error) => {
+    console.log('Connection error:', error);
+});
+
+// Set up readline for user input
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+// Function to send messages
+function sendMessage() {
+    rl.question("You: ", (content) => {
+        if (content) {
+            const messageData = { senderId: '60d21b4667d0d8992e610c85', content };
+            socket.emit('sendMessage', messageData); // Emit the entire messageData object
+            console.log('Sent message:', messageData);
+        }
+        sendMessage(); // Call again to allow for continuous messaging
+    });
+}
+
+// Start sending messages
+sendMessage();
+
+// Handle disconnection
+socket.on('disconnect', (reason) => {
+    console.log('Disconnected:', reason);
+});
--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -1,8 +1,10 @@
 import { Server } from 'socket.io';
-import User from "../models/Users.js";
 import chatController from "../controllers/chatController.js";
 export default function (server) {
-    const io = new Server(server);
+    const io = new Server(server,{
+        cors: {
+            origin: "*", methods: ["GET", "POST"]
+        }});
 
     io.on('connection', (socket) => {
         console.log('New client connected: ' + socket.id);
@@ -11,6 +13,7 @@ export default function (server) {
             try {
                 const { senderId, content } = data;
                 const message = await chatController.createMessage(content, senderId);
+                console.log('Message created:', message); // Log created message
                 io.emit('newMessage', message);
             } catch (err) {
                 console.error('Error saving message:', err);
